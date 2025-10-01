1df9802bd86b0dc3616eae9cec77f46e988c4327
TwBenjaminVargas <benjaminvargasoficial@gmail.com>
2025-09-30 23:16:23 -0300

    modify - restcontroller profile

--- src/main/java/ar/edu/iua/iw3/integration/cli2/model/controllers/ProductCli2RestController.java
+++ src/main/java/ar/edu/iua/iw3/integration/cli2/model/controllers/ProductCli2RestController.java
@@ -36,8 +36,8 @@ import lombok.extern.slf4j.Slf4j;
 @RestController
 @RequestMapping(Constants.URL_INTEGRATION_CLI2 + "/products")
 @Slf4j
-@Profile("cli2")
-//@Profile("mysqlprod")
+//@Profile("cli2")
+@Profile("mysqlprod")
 public class ProductCli2RestController extends BaseRestController {
 
 	@Autowired
